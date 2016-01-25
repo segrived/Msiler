aa7e417cf1069fce714a0700622c58f93d0c12a7
Babaev Evgeniy <segrived@gmail.com>
2016-01-25 05:26:53 +0200

    Autoscroll to selected function in Msiler window

---
+++ Msiler/ListBoxScroll.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Quart.Msiler
+{
+    class ListBoxScroll : ListBox
+    {
+        public ListBoxScroll() : base() {
+            SelectionChanged += new SelectionChangedEventHandler(ListBoxScroll_SelectionChanged);
+        }
+
+        void ListBoxScroll_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            ScrollIntoView(SelectedItem);
+        }
+    }
+}
