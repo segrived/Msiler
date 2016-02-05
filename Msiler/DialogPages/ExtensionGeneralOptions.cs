52406e297c65785d147ba67f2d70ed57f63c6a8c
Babaev Evgeniy <segrived@gmail.com>
2016-02-05 09:38:24 +0200

    Add missing file

---
+++ Msiler/DialogPages/ExtensionGeneralOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Msiler.DialogPages
+{
+    public class ExtensionGeneralOptions : MsilerDialogPage
+    {
+        [Category("General")]
+        [DisplayName("Update listing only if toolbox is visible")]
+        [Description("Bytecode listing  will be updated, only if Msiler toolbox visible on screen, it can reduce compilation times.")]
+        public bool UpdateListingOnlyIfVisible { get; set; } = true;
+
+        [Category("General")]
+        [DisplayName("Follow selected function in editor")]
+        [Description("")]
+        public bool FollowSelectedFunctionInEditor { get; set; } = false;
+    }
+
+}
