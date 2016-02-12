cca201bf98778836894ecbe07fe69c72a67dfa34
Babaev Evgeniy <segrived@gmail.com>
2016-02-12 13:59:26 +0200

    Version bump

--- Msiler/MsilerPackage.cs
+++ Msiler/MsilerPackage.cs
@@ -17,7 +17,7 @@ namespace Msiler
     };
 
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "2.1-beta1", IconResourceID = 400)]
+    [InstalledProductRegistration("#110", "#112", "2.1-rc1", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideToolWindow(typeof(MsilerToolWindow), MultiInstances = false)]
     [ProvideOptionPage(typeof(ExtensionGeneralOptions), "Msiler", "General", 0, 0, true)]
